d787b72f7d56d0d57893e9094160d62549a84323
HzLim <dlaghddls99@gmail.com>
2026-08-14 13:40:17 +0900

    page 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // images: { unoptimized: true }, // GitHub Pages 배포 시 이미지 에러 방지를 위해 필요할 수 있습니다
+};
 
 export default nextConfig;
